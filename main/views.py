b3881264888cb0b55df98772dfb5e34709a3d64d
ErnestoAizenberg <sereernst@gmail.com>
2025-06-15 20:22:23 +0300

    feat(related articles): add DTL component to dynamicly render related articles

--- main/views.py
+++ main/views.py
@@ -8,12 +8,13 @@ def index(request):
 
 def article(request, slug):
     article = get_object_or_404(Article, slug=slug, status='published')
+    related_articles = Article.objects.filter(status='published').order_by('-created_at')
     reading_spead = 200
     word_count = len(article.content.split())
     reading_time_minutes = max(1, round(word_count / reading_spead))
     reading_time = f"{reading_time_minutes} мин чтения"
 
-    return render(request, 'main/article.html', {'article': article, 'reading_time': reading_time})
+    return render(request, 'main/article.html', {'article': article, 'reading_time': reading_time, "related_articles": related_articles})
 
 def test(request):
     return render(request, 'main/article0.html')
