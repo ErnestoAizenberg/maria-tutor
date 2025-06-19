a132ca2bad7fff135ae3fef88f660699c1e985d1
ErnestoAizenberg <sereernst@gmail.com>
2025-06-19 07:14:10 +0300

    feat: add forms handling[D

---
+++ main/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+
+class AplicatuionForm(forms.Form):
+    pass
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Article
 
 
@@ -14,7 +14,7 @@ def article(request, slug):
     reading_time_minutes = max(1, round(word_count / reading_spead))
     reading_time = f"{reading_time_minutes} мин чтения"
 
-    return render(request, 'main/article.html', {'article': article, 'reading_time': reading_time, "related_articles": related_articles})
+    return render(request, 'main/article-purple.html', {'article': article, 'reading_time': reading_time, "related_articles": related_articles})
 
 def articles(request):
     list_articles = Article.objects.filter(status='published').order_by('-created_at')
@@ -27,3 +27,43 @@ def articles(request):
 
 def test(request):
     return render(request, 'main/article0.html')
+
+def apply_success(request):
+    return render(request, 'main/apply_success.html')
+
+def application_submit(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        goal = request.POST.get('goal')
+
+        if not name or not email:
+            return render(request, 'index-purple.html', {'error': 'Пожалуйста заполните все поля(('})
+
+        return redirect('apply_success')
+    else:
+        pass
+
+def connect_request(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        return redirect('connect_success')
+    else:
+        pass
+
+def connect_success(request):
+    if request.method == 'GET':
+        return render(request, 'main/connect_success.html')
+    else:
+        pass
+
+def email_subscribe_success(request):
+    return render(request, 'main/email_subscribe_success.html')
+
+def subscribe_email(request):
+    if request.method == 'POST':
+        return redirect('email_subscribe_success')
