d68db83e48b9315a980210356e5a2fa1b7dab0f4
Ernesto <sereernest@gmail.com>
2026-03-08 11:09:03 +0300

    refactor markdown_to_html function

--- main/templatetags/markdown_tags.py
+++ main/templatetags/markdown_tags.py
@@ -6,28 +6,22 @@ from django.utils.safestring import mark_safe
 register = template.Library()
 
 
-@register.filter(is_safe=True)  # Ключевой параметр!
-def markdown_to_html(text):
+@register.filter(is_safe=True)
+def markdown_to_html(text: str) -> str:
+    """Convert makdown to safe HTML"""
     if not text:
         return ""
 
-    # 1. Отключаем автоэкранирование для исходного текста
     text = mark_safe(text)
-
-    # 2. Обработка Markdown
     md = markdown.Markdown(
-        extensions=["extra", "nl2br", "sane_lists"], output_format="html5"
+        extensions=["extra", "nl2br", "sane_lists"], output_format="html"
     )
-
-    # 3. Парсинг HTML с сохранением структуры
     soup = BeautifulSoup(md.convert(text), "html.parser")
 
-    # 4. Двойная обработка Markdown внутри HTML-тегов
-    for element in soup.find_all(text=True):
-        if element.parent.name not in ["script", "style"]:
+    for element in soup.find_all(string=True):
+        if element.parent and element.parent.name not in ["script", "style"]:
             html = md.reset().convert(element.string)
             element.replace_with(BeautifulSoup(html, "html.parser"))
 
-    # 5. Возвращаем как безопасный HTML
     result = str(soup)
     return mark_safe(result)
